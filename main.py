64c70d1e197ae8fb967c35e4a26db27ad320936a
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-05-26 16:34:15 +0900

    Refactor: Use image-based Gemini for PDF analysis and improve environment handling

--- main.py
+++ main.py
@@ -1,38 +1,25 @@
-from pdf2image import convert_from_path
-import easyocr
-import cv2
-import subprocess
-from kaggle_secrets import UserSecretsClient
 from google import genai
+from google.genai import types
+from pdf2image import convert_from_path
+from dotenv import dotenv_values
 
-subprocess.run(["apt-get", "update"])
-subprocess.run(["apt-get", "install", "-y", "poppler-utils"])
 
-reader = easyocr.Reader(["en", "ja"])
-path = "/kaggle/input/test-pdf/example.pdf" # Kaggle 上で Dataset を作成して、Notebook から設定する必要がある
-pages = convert_from_path(path, dpi=300)
-text = ""
+path = "example.pdf"
+page = convert_from_path(path, dpi=300)[0]
+image = page.rotate(270, expand=True)
+image.save("example.jpeg")
+image_bytes = image.tobytes()
 
-for i, page in enumerate(pages):
-    page = page.rotate(270, expand=True)
-    page.save(f"page_{i + 1}.jpg")
-    image = cv2.imread(f"page_{i + 1}.jpg")
-    result = reader.readtext(image, detail=0)
-    print(f"DEBUG OCR result: {result}")
-    text += "\n".join(result) + "\n"
-
-with open("example.txt", "w") as f:
-    f.write(text)
-
-user_secrets = UserSecretsClient()
-gemini_api_key = user_secrets.get_secret("GEMINI_API_KEY")
-
-client = genai.Client(api_key=gemini_api_key)
+config = dotenv_values(".env")
+api_key = config.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
 
 response = client.models.generate_content(
     model="gemini-2.0-flash",
-    contents="extract keywords from the following parsed pdf" + text,
+    contents=[
+        types.Part.from_bytes(data=image_bytes, mime_type="image/jpeg"),
+        "explanation of sorting",
+    ],
 )
 
-with open("response.txt", "w") as f:
-    f.write(str(response))
+print(response.text)
