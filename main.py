54cda7f78b2a96226183f5910630911d122a335a
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-05-26 15:57:11 +0900

    feat: Integrate Gemini API for keyword extraction

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from pdf2image import convert_from_path
 import easyocr
 import cv2
 import subprocess
+from kaggle_secrets import UserSecretsClient
+from google import genai
 
 subprocess.run(["apt-get", "update"])
 subprocess.run(["apt-get", "install", "-y", "poppler-utils"])
 
 reader = easyocr.Reader(["en", "ja"])
-path = "/kaggle/input/test-pdf/example.pdf"
+path = "/kaggle/input/test-pdf/example.pdf" # Kaggle 上で Dataset を作成して、Notebook から設定する必要がある
 pages = convert_from_path(path, dpi=300)
 text = ""
 
@@ -21,3 +23,16 @@ for i, page in enumerate(pages):
 
 with open("example.txt", "w") as f:
     f.write(text)
+
+user_secrets = UserSecretsClient()
+gemini_api_key = user_secrets.get_secret("GEMINI_API_KEY")
+
+client = genai.Client(api_key=gemini_api_key)
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    contents="extract keywords from the following parsed pdf" + text,
+)
+
+with open("response.txt", "w") as f:
+    f.write(str(response))
