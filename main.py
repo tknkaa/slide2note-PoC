3bc1ae36ea0b6f7be10fc65d4c33d9adf6f9551c
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-05-26 22:01:48 +0900

    feat: Generate explanation of image with Gemini and save results

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ from dotenv import dotenv_values
 path = "example.pdf"
 page = convert_from_path(path, dpi=300)[0]
 image = page.rotate(270, expand=True)
-image.save("example.jpeg")
-image_bytes = image.tobytes()
+image.save("example.jpg")
+with open('example.jpg', 'rb') as f:
+    image_bytes = f.read()
+# image_bytes = image.tobytes()
 
 config = dotenv_values(".env")
 api_key = config.get("GEMINI_API_KEY")
@@ -17,9 +19,10 @@ client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
     model="gemini-2.0-flash",
     contents=[
-        types.Part.from_bytes(data=image_bytes, mime_type="image/jpeg"), # invalid
+        types.Part.from_bytes(data=image_bytes, mime_type="image/jpeg"),
         "explanation of sorting",
     ],
 )
 
-print(response.text)
+with open("example.txt", "x") as f:
+    f.write(response.text)
