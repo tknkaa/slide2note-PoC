3dff8f8661b7fe70b88e91b00592673162881743
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-05-29 23:57:21 +0900

    refactor: useActionState for form

---
+++ web/app/form.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useActionState } from "react";
+import { getGeminiResponse, type GeminiResponse } from "./actions";
+
+export default function Form() {
+  const initialState: GeminiResponse = { filename: "", response: "" }
+  const [response, action, isPending] = useActionState(getGeminiResponse, initialState)
+
+  return (
+    <>
+      <form action={action}>
+        <input type="file" accept=".pdf" name="file" />
+        <button type="submit">submit</button>
+      </form>
+      {isPending ? (
+        <div>loading</div>
+      ) : (
+        <div>response: {response.response}</div>
+      )}
+    </>
+  )
+}
--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,39 +1,7 @@
-"use client";
-
-import type React from "react";
-import { useState } from "react";
+import Form from "./form"
 
 export default function Home() {
-	const [file, setFile] = useState<File | null>(null);
-	const [message, setMessage] = useState("");
-	const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		if (event.target.files) {
-			setFile(event.target.files[0]);
-		}
-	};
-
-	const handleUpload = async () => {
-		const formData = new FormData();
-		if (file) {
-			formData.append("file", file);
-		}
-		const res = await fetch("http://127.0.0.1:8000/slide", {
-			method: "POST",
-			body: formData,
-		});
-		const json = await res.json();
-		setMessage(json.response);
-	};
-
 	return (
-		<div>
-			<form action={handleUpload}>
-				<input type="file" accept=".pdf" onChange={handleFileChange} />
-				<button type="submit">
-					Upload
-				</button>
-			</form>
-			<div>message: {message}</div>
-		</div>
-	);
+		<Form />
+	)
 }
